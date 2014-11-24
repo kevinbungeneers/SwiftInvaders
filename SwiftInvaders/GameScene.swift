4b8feb9e2b1b43272c4a9de96455e8b28506e6ed
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2014-11-24 22:20:45 +0100

    Added player to scene + movement

--- SwiftInvaders/GameScene.swift
+++ SwiftInvaders/GameScene.swift
@@ -9,11 +9,58 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var player: SKSpriteNode?
+    
+    var isMovingLeft: Bool = false
+    var isMovingRight: Bool = false
+    var isShooting: Bool = false
+
+    
     override func didMoveToView(view: SKView) {
-        
+        if let childNode = self.childNodeWithName("player") as? SKSpriteNode {
+            self.player = childNode
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        /* Handle movement, like a boss */
+        var newPosition = self.player?.position
+        if self.isMovingLeft {
+            newPosition!.x -= 10
+            if newPosition!.x < (self.player?.frame.width)! / 2 {
+                newPosition!.x = (self.player?.frame.width)! / 2
+            }
+        }
+        
+        if self.isMovingRight {
+            newPosition!.x += 10
+            if newPosition!.x > (self.frame.width - (self.player?.frame.width)! / 2) {
+                newPosition!.x = self.size.width - (self.player?.frame.width)! / 2
+            }
+        }
+        
+        self.player?.position = newPosition!
+    }
+    
+    override func keyDown(theEvent: NSEvent) {
+        self.handleInput(theEvent.keyCode, active: true)
+    }
+    
+    override func keyUp(theEvent: NSEvent) {
+        self.handleInput(theEvent.keyCode, active: false)
+    }
+    
+    func handleInput(keyCode: UInt16, active: Bool) {
+        switch keyCode {
+        case 123:
+            self.isMovingLeft = active
+        case 124:
+            self.isMovingRight = active
+        case 49:
+            self.isShooting = active
+        default:
+            ()
+        }
     }
 }
