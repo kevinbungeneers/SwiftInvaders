b9841adcd9c80b95281e19d857826448279b48f5
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2014-11-24 23:11:53 +0100

    Refactored some code + load enemies from Level1 scene

--- SwiftInvaders/GameScene.swift
+++ SwiftInvaders/GameScene.swift
@@ -24,39 +24,17 @@ class GameScene: SKScene {
         if let childNode = self.childNodeWithName("player") as? SKSpriteNode {
             self.player = childNode
         }
+        
+        if let children = GameScene.unarchiveFromFile("Level1")?.children {
+            for child: AnyObject in children {
+                self.addChild(child.copy() as SKNode)
+            }
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Handle movement, like a boss */
-        var newPosition = self.player?.position
-        if self.isMovingLeft {
-            newPosition!.x -= 10
-            if newPosition!.x < (self.player?.frame.width)! / 2 {
-                newPosition!.x = (self.player?.frame.width)! / 2
-            }
-        }
-        
-        if self.isMovingRight {
-            newPosition!.x += 10
-            if newPosition!.x > (self.frame.width - (self.player?.frame.width)! / 2) {
-                newPosition!.x = self.size.width - (self.player?.frame.width)! / 2
-            }
-        }
-        
-        self.player?.position = newPosition!
-        
-        /* Handle shooting, like a boss */
-        if self.isShooting && (currentTime - self.lastShotFired > 0.3) {
-            var shotPosition = self.player?.position
-            shotPosition!.y += 65
-            let newShot = shot.copy() as SKSpriteNode
-            newShot.position = shotPosition!
-            self.addChild(newShot)
-            
-            let action: SKAction = SKAction.moveByX(0, y: 600, duration: 1.0)
-            newShot.runAction(SKAction.repeatActionForever(action))
-            self.lastShotFired = currentTime;
-        }
+        self.handleMovement()
+        self.handleShooting(currentTime)
     }
     
     override func keyDown(theEvent: NSEvent) {
@@ -77,6 +55,41 @@ class GameScene: SKScene {
             self.isShooting = active
         default:
             ()
+        }
+    }
+    
+    func handleMovement() {
+        /* Handle movement, like a boss */
+        var newPosition = self.player?.position
+        if self.isMovingLeft {
+            newPosition!.x -= 10
+            if newPosition!.x < (self.player?.frame.width)! / 2 {
+                newPosition!.x = (self.player?.frame.width)! / 2
+            }
+        }
+        
+        if self.isMovingRight {
+            newPosition!.x += 10
+            if newPosition!.x > (self.frame.width - (self.player?.frame.width)! / 2) {
+                newPosition!.x = self.size.width - (self.player?.frame.width)! / 2
+            }
+        }
+        
+        self.player?.position = newPosition!
+    }
+    
+    func handleShooting(currentTime: CFTimeInterval) {
+        /* Handle shooting, like a boss */
+        if self.isShooting && (currentTime - self.lastShotFired > 0.3) {
+            var shotPosition = self.player?.position
+            shotPosition!.y += 65
+            let newShot = shot.copy() as SKSpriteNode
+            newShot.position = shotPosition!
+            self.addChild(newShot)
+            
+            let action: SKAction = SKAction.moveByX(0, y: 600, duration: 1.0)
+            newShot.runAction(SKAction.repeatActionForever(action))
+            self.lastShotFired = currentTime;
         }
     }
 }
